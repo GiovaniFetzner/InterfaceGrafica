a122e1600891e8c084da965591c2f4a95df99ac3
Giovani <giovanifetzner01@gmail.com>
2023-09-03 16:52:49 -0300

    Interface realizando soma (Somente numeros) OBS: Corrigir caso entrada for letras

---
+++ src/Tela.java
@@ -0,0 +1,47 @@
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class Tela extends JFrame{
+    private JButton clickHereButton;
+    private JTextField textField1;
+    private JPanel Painel;
+    private JLabel lblEntrada1;
+    private JTextField textField2;
+    private JLabel lblEntrada2;
+    private JLabel lblTotal;
+    private String entrada1, entrada2, saida;
+    private int total;
+
+
+    public Tela(){
+        lblEntrada1.setVisible(false);
+        lblEntrada2.setVisible(false);
+
+        clickHereButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                entrada1 = textField1.getText();
+                lblEntrada1.setText(entrada1);
+                entrada2 = textField2.getText();
+                lblEntrada2.setText(entrada2);
+                total = Integer.parseInt(entrada1) + Integer.parseInt(entrada2);
+                saida = Integer.toString(total);
+                lblTotal.setText(saida);
+
+            }
+        });
+
+    }
+
+    public static void main(String[] args) {
+        Tela tela = new Tela();
+        tela.setContentPane(tela.Painel);
+        tela.setTitle("Calculadora");
+        tela.setSize(400,150);
+        tela.setVisible(true);
+        tela.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
+
+
+}
